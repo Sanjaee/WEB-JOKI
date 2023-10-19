0d0dde8f0dd0f6dfb7daeedd6e5aaa68f6c1544e
Sanjaee <afrizaahmad18@gmail.com>
2023-10-19 17:09:02 +0700

    coba lg 2

--- src/Components/Tutorial.jsx
+++ src/Components/Tutorial.jsx
@@ -1,13 +1,56 @@
-export default function Tutorial(){
-    return(
-        <div>
-            <h1 className=" text-center font-bold text-4xl mt-20">Cara Pemesanan</h1>
-            <div className=" justify-center items-center flex mb-10">
-                <video controls className=" w-[600px] h-[400px] rounded-xl">
-                    <source src="AtengJoki.mp4" type="video/mp4" className=" rounded-xl"/>
-                </video>
+import React, { useState } from "react";
 
-            </div>
-        </div>
-    )
+function ThumbnailVideo({ onClick }) {
+  return (
+    <div>
+      <img
+        src="thumnel.png"
+        alt="Video Thumbnail"
+        className=" w-[600px] h-[400px] rounded-xl mt-20"
+        onClick={onClick}
+        style={{ cursor: "pointer" }}
+      />
+    </div>
+  );
+}
+
+function FullVideo({ onBack }) {
+  return (
+    <div>
+      <video controls className="w-[600px] h-[400px] rounded-xl">
+        <source src="AtengJoki.mp4" type="video/mp4" className="rounded-xl" />
+      </video>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md mx-auto block mt-4"
+        onClick={onBack}
+      >
+        Berhenti Menonton
+      </button>
+    </div>
+  );
+}
+
+export default function Tutorial() {
+  const [showFullVideo, setShowFullVideo] = useState(false);
+
+  const handleVideoClick = () => {
+    setShowFullVideo(true);
+  };
+
+  const handleBackToThumbnail = () => {
+    setShowFullVideo(false);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center font-bold text-4xl mt-20">Cara Pemesanan</h1>
+      <div className="justify-center items-center flex mb-10">
+        {showFullVideo ? (
+          <FullVideo onBack={handleBackToThumbnail} />
+        ) : (
+          <ThumbnailVideo onClick={handleVideoClick} />
+        )}
+      </div>
+    </div>
+  );
 }
